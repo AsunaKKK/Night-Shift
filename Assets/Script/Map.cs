92edffe092464914a361bf12aea436d3143ece30
AsunaKKK <aiaicz99@gmail.com>
2023-03-29 01:04:35 +0700

    Map

--- Assets/Script/Map.cs
+++ Assets/Script/Map.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class Map : MonoBehaviour
 {
-    public Button button;
+    //public Button button;
     public RectTransform uiElement;
     public float xOffset;
     public float yOffset;
@@ -13,23 +13,36 @@ public class Map : MonoBehaviour
     public float _xOffset;
     public float _yOffset;
 
+    public bool _isDragging = true;
+
 
     //Set Button
     void Start()
     {
-        button.onClick.AddListener(MoveElement);
+        //button.onClick.AddListener(MoveElement);
     }
 
     //Move Mini Map Come Show
     public void MoveElement()
     {
-        Vector2 currentPosition = uiElement.anchoredPosition;
-        uiElement.anchoredPosition = new Vector2(currentPosition.x + xOffset, currentPosition.y + yOffset);
+        if(_isDragging)
+        {
+            Vector2 currentPosition = uiElement.anchoredPosition;
+            uiElement.anchoredPosition = new Vector2(currentPosition.x + xOffset, currentPosition.y + yOffset);
+            Time.timeScale = 0;
+            _isDragging = false;
+        }
+        
     }
     // Move Mini Map Out
     public void ResetMove()
     {
-        Vector2 currentPosition = uiElement.anchoredPosition;
-        uiElement.anchoredPosition = new Vector2(currentPosition.x + _xOffset, currentPosition.y + _yOffset);
+        if(!_isDragging)
+        {
+            Vector2 currentPosition = uiElement.anchoredPosition;
+            uiElement.anchoredPosition = new Vector2(currentPosition.x + _xOffset, currentPosition.y + _yOffset);
+            Time.timeScale = 1;
+            _isDragging = true;
+        }
     }
 }
