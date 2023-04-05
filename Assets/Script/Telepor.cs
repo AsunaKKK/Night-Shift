34bcd4d57d20bdcd4890be97eaf20d0a999b42c1
AsunaKKK <aiaicz99@gmail.com>
2023-04-06 03:45:26 +0700

    EditK

---
+++ Assets/Script/Telepor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telepor : MonoBehaviour
+{
+    [SerializeField] private Transform destination;
+
+    public GameObject smybool;
+
+    private void Start()
+    {
+        smybool.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            smybool.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            smybool.SetActive(false);
+        }
+    }
+
+    //Set Value Player
+    public Transform GetDestination()
+    {
+        return destination;
+    }
+}
