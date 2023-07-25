bbc6e68a349b0e6df4a67666256a7e6569d0b9d4
AsunaKKK <aiaicz99@gmail.com>
2023-07-25 08:47:21 +0700

    Game

--- Assets/Script/PlayerTelepot.cs
+++ Assets/Script/PlayerTelepot.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerTelepot : MonoBehaviour
 {
     private GameObject currentTeleporter;
+    public Image blackImgTeleport;
+
+    private bool isFading;
+
+    private void Start()
+    {
+        SetOpacity(0f);
+        blackImgTeleport.gameObject.SetActive(false);
+    }
 
     void Update()
     {
@@ -13,8 +23,43 @@ public class PlayerTelepot : MonoBehaviour
             if (currentTeleporter != null)
             {
                 transform.position = currentTeleporter.GetComponent<Telepor>().GetDestination().position;
+
+                StartCoroutine(FadeOut());
             }
         }
+    }
+
+    IEnumerator FadeOut()
+    {
+        if (isFading)
+            yield break;
+
+        blackImgTeleport.gameObject.SetActive(true);
+        isFading = true;
+        SetOpacity(1f);
+
+
+        yield return new WaitForSeconds(0.1f); 
+        float duration = 1f;
+        float elapsedTime = 0f;
+        float startOpacity = 1f;
+        while (elapsedTime < duration)
+        {
+            float alpha = Mathf.Lerp(startOpacity, 0f, elapsedTime / duration);
+            SetOpacity(alpha);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        SetOpacity(0f);
+        blackImgTeleport.gameObject.SetActive(false);
+        isFading = false;
+    }
+
+    public void SetOpacity(float opacity)
+    {
+        Color color = blackImgTeleport.color;
+        color.a = opacity;
+        blackImgTeleport.color = color;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
