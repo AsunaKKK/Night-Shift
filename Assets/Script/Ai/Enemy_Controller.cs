41f9c2aca89deb96d721d7eb278b00f1628a47d6
MelwNump <125186451+MelwNump@users.noreply.github.com>
2023-08-15 07:10:01 +0700

    EnemyAttack

--- Assets/Script/Ai/Enemy_Controller.cs
+++ Assets/Script/Ai/Enemy_Controller.cs
@@ -1,67 +1,71 @@
 using UnityEngine;
+using System.Collections;
 
 public class Enemy_Controller : MonoBehaviour
 {
+    //Move
     public Waypoint[] wayPoints;
     public float speed = 5f;
     public bool isCircular;
     public bool inReverse = true;
     public bool canWalk = true;
-    
     public float runRange = 2f;
-   
     private Waypoint currentWaypoint;
     private int currentIndex = 0;
     private bool isWaiting = false;
-    private float originalSpeed;
-
-    public bool isChasingPlayer = false;
+    private float originalSpeed; 
     private Vector3 playerPosition;
+
+
     
-    //animation
+    
+    // Attack
+    private bool isChasingPlayer = false;
+   
+    private bool isAttacking = false;
+    public float attackRange = 4f;
+    public int attackDamage = 10;
+    public float attackCooldown = 2f;
+
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator anim;
 
     private enum State { Idle, Walk, Run, Attack }
     private State state = State.Idle;
 
-    void Start()
+    private void Start()
     {
         if (wayPoints.Length > 0)
         {
             currentWaypoint = wayPoints[0];
         }
         originalSpeed = speed;
-
         anim = GetComponent<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
         UpdateAnimation(state);
+
         if (canWalk && !isChasingPlayer && currentWaypoint != null && !isWaiting)
         {
-            // Check if the player is within range
             GameObject player = GameObject.FindGameObjectWithTag("Player");
             if (player != null && Vector3.Distance(transform.position, player.transform.position) < runRange)
             {
-                // Call the Run function if the player is within range
                 Run(player.transform.position);
             }
             else
             {
-                // Move towards the current waypoint if the player is not within range
                 MoveTowardsWaypoint();
             }
         }
         else if (isChasingPlayer)
         {
-            // Move towards the player if we are chasing them
             MoveTowardsPlayer();
         }
     }
 
-    void Pause()
+    private void Pause()
     {
         isWaiting = !isWaiting;
         state = State.Idle;
@@ -72,28 +76,17 @@ public class Enemy_Controller : MonoBehaviour
         Vector3 currentPosition = transform.position;
         Vector3 targetPosition = currentWaypoint.transform.position;
 
-        // Only move along the x-axis (left or right)
         targetPosition.y = currentPosition.y;
         targetPosition.z = currentPosition.z;
 
         if (Vector3.Distance(currentPosition, targetPosition) > 0.1f)
         {
             Vector3 directionOfTravel = (targetPosition - currentPosition).normalized;
-
             transform.Translate(directionOfTravel * speed * Time.deltaTime, Space.World);
             state = State.Walk;
 
-            // Flip the enemy along the x-axis based on the movement direction
-            if (directionOfTravel.x < 0)
-            {
-                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            }
-            else if (directionOfTravel.x > 0)
-            {
-                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            }
+            FlipEnemy(directionOfTravel.x);
         }
-        
         else
         {
             if (currentWaypoint.waitSeconds > 0)
@@ -112,7 +105,20 @@ public class Enemy_Controller : MonoBehaviour
             }
 
             NextWaypoint();
-           // state = State.Idle;
+        }
+
+       
+    }
+
+    private void FlipEnemy(float directionX)
+    {
+        if (directionX < 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else if (directionX > 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
     }
 
@@ -143,17 +149,11 @@ public class Enemy_Controller : MonoBehaviour
 
     private void Run(Vector3 playerPosition)
     {
-        // Set the speed to a higher value for running
         speed = 8f;
-
-        // Set the current waypoint to the player's position
         currentWaypoint.transform.position = playerPosition;
-
-        // Start chasing the player
         isChasingPlayer = true;
         this.playerPosition = playerPosition;
         state = State.Run;
-
     }
 
     private void MoveTowardsPlayer()
@@ -161,25 +161,60 @@ public class Enemy_Controller : MonoBehaviour
         if (Vector3.Distance(transform.position, playerPosition) > 0.1f)
         {
             Vector3 directionOfTravel = (playerPosition - transform.position).normalized;
-            transform.Translate(
-           directionOfTravel * speed * Time.deltaTime,Space.World );
-            //state = State.Run;
-
-
+            transform.Translate(directionOfTravel * speed * Time.deltaTime, Space.World);
+            FlipEnemy(directionOfTravel.x);
         }
         else
         {
-            // Stop chasing the player when we reach their position
             isChasingPlayer = false;
             speed = originalSpeed;
             currentWaypoint = wayPoints[currentIndex];
         }
+
+        if (Vector3.Distance(transform.position, playerPosition) < attackRange)
+        {
+            AttackPlayer();
+        }
     }
 
-    //Animation Updeat
+    private void AttackPlayer()
+    {
+        if (!isAttacking)
+        {
+            isAttacking = true;
+            state = State.Attack;
+
+            // Check if the player is within attack range
+            if (Vector3.Distance(transform.position, playerPosition) < attackRange)
+            {
+                // Access the player GameObject
+                GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+                if (player != null)
+                {
+                    // Access the player's script
+                    PlayerController playerController = player.GetComponent<PlayerController>();
+
+                    if (playerController != null)
+                    {
+                        // Deal damage to the player
+                        playerController.TakeDamage(attackDamage);
+                    }
+                }
+            }
+
+            StartCoroutine(StartAttackCooldown());
+        }
+    }
+
+    private IEnumerator StartAttackCooldown()
+    {
+        yield return new WaitForSeconds(attackCooldown);
+        isAttacking = false;
+    }
+
     private void UpdateAnimation(State newState)
     {
-        
         anim.SetInteger("state", (int)newState);
     }
 }
